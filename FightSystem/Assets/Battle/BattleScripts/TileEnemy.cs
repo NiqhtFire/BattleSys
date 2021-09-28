3401ebcdb09ce871311622fffa917a273260ebcd
NiqhtFire <66108534+NiqhtFire@users.noreply.github.com>
2021-09-28 19:30:47 +0300

    v1

---
+++ FightSystem/Assets/Battle/BattleScripts/TileEnemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileEnemy : MonoBehaviour
+{
+    public SpriteRenderer highlightSprite;
+    public Color validColor;
+    public Color wrongColor;
+
+   
+}
